57973a6299f500f9c107a6202f8cca75956fbd39
Guilherme Vicentin <gvicentin@ciandt.com>
2022-09-17 17:33:40 -0300

    Testing step motor

--- br/main.c
+++ br/main.c
@@ -1,34 +1,56 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdint.h>
 #include "pico/stdlib.h"
 #include "log.h"
-#include "mpu6050.h"
+
+#define IN_1 0
+#define IN_2 1
+#define IN_3 2
+#define IN_4 3
+
+static uint32_t m_bit_mask;
+static uint32_t m_sequence_mask[4];
+
+void step(uint32_t steps);
 
 int main(void) {
     stdio_init_all();
 
-    float gyro[3], accel[3];
+    // Init GPIO
+    m_bit_mask = (1 << IN_1) | (1 << IN_2) |
+                 (1 << IN_3) | (1 << IN_4);
+    gpio_init_mask(m_bit_mask);
+    gpio_set_dir_out_masked(m_bit_mask);
 
-    // init mpu
-    mpu6050_config mpu_config = {
-        .i2c_inst = i2c_default,
-        .i2c_sda_pin = PICO_DEFAULT_I2C_SDA_PIN,
-        .i2c_scl_pin = PICO_DEFAULT_I2C_SCL_PIN
-    };
-    mpu6050_init(mpu_config);
-    mpu6050_set_gyro_fs(MPU6050_GFS_250);
-    mpu6050_set_accel_fs(MPU6050_AFS_2G);
+    // Init sequence steps
+    bool fullstep = true;
+    if (fullstep) {
+        m_sequence_mask[0] = (1 << IN_1) | (1 << IN_4);
+        m_sequence_mask[1] = (1 << IN_1) | (1 << IN_2);
+        m_sequence_mask[2] = (1 << IN_2) | (1 << IN_3);
+        m_sequence_mask[3] = (1 << IN_3) | (1 << IN_4);
+    }
+    else {
+        m_sequence_mask[0] = (1 << IN_1);
+        m_sequence_mask[1] = (1 << IN_2);
+        m_sequence_mask[2] = (1 << IN_3);
+        m_sequence_mask[3] = (1 << IN_4);
+    }
+
+    gpio_put_masked(m_bit_mask, m_sequence_mask[0]);
 
     while (true) {
-        LOG_ERROR("This is a error log");
-        LOG_DEBUG("This is a debug log");
-
-        mpu6050_read_gyro(gyro);
-        mpu6050_read_accel(accel);
-        SP_RAW_SENSORS(gyro, accel);
-        
-        sleep_ms(20);
+        step(4096);
+        sleep_ms(2500);
     }
 
     return 0;
+}
+
+void step(uint32_t steps) {
+    for (uint32_t i = 0; i < steps; i++) {
+        gpio_put_masked(m_bit_mask, m_sequence_mask[i % 4]);
+        busy_wait_us(1300);
+    }
 }
