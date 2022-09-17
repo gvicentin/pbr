9f75f450029e4ed0cb05e470bf338c07f7b93516
Guilherme Vicentin <gvicentin@ciandt.com>
2022-09-16 21:02:36 -0300

    Removed unused files

--- br/main.c
+++ br/main.c
@@ -3,27 +3,9 @@
 #include "pico/time.h"
 #include "pico/stdlib.h"
 
-#include "imu.h"
-
 int main(void) {
     stdio_init_all();
 
-    imu_config imu_conf = {
-        .accl_scale = IMU_FS_0,
-        .gyro_scale = IMU_FS_1,
-        .init_calibration = true
-    };
-    imu_init(imu_conf);
-
-    float accl[3], temp, gyro[3];
-
-    while (true) {
-        imu_read(accl, &temp, gyro);
-        // printf("%f %f %f %f %f %f\n", accl[0], accl[1], accl[2],
-        //                               gyro[0], gyro[1], gyro[2]);
-        printf("%f %f %f\n", gyro[0], gyro[1], gyro[2]);
-        sleep_ms(200);
-    }
 
     return 0;
 }
